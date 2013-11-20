9398662692bd17569e4d08f611f71e3f53507ba0
Marco Loche <marco.loche@capturator.com>
2013-11-20 22:05:59 +0100

    add code for monito

--- 06_monitor/Blink/Blink.ino
+++ 06_monitor/Blink/Blink.ino
@@ -1,6 +1,6 @@
 /*
-  functions
-  Increase and decrease intensity of an LED 
+  Serial monitor
+  Monitoring value and printing debugging messages 
   This example code is part of the AUG-ROMA Arduino Course
   This example code is in the public domain.
   Author Marco Loche
@@ -9,27 +9,43 @@
 // Pin 9 is a PWM PIN
 const int led = 9;
 // Define the value of brightness of the led
-int intensity = 0;    
+int intensity = 0; 
+boolean fadeIn = true;  
 
 void setup() {                
-  pinMode(led, OUTPUT);     
-}
-
-void fadeIn() {
-  for (intensity = 0 ; intensity <= 255; intensity++) { 
-    analogWrite(led, intensity);
-    delay(20);                     
-  } 
-}
-
-void fadeOut() {
-  for(intensity = 255 ; intensity >= 0; intensity--) {
-    analogWrite(led, intensity);
-    delay(20);                      
-  } 
+  pinMode(led, OUTPUT);  
+  // initialize serial communication at 9600 bits per second: 
+  // Check the value in the Monitor 
+  Serial.begin(9600);
+  Serial.println("\nReady\n\n");
 }
 
 void loop() {
-  fadeIn();
-  fadeOut();
+  
+  if(intensity%5 == 0) {
+    Serial.print(intensity); 
+    Serial.print(" "); 
+  }
+  if(fadeIn){
+     //Fade in
+    analogWrite(led, intensity);
+    intensity++;  
+    delay(20);               
+  }
+  else{
+    //Fade Out
+    analogWrite(led, intensity);
+    intensity--;
+    delay(20);
+  }
+  
+  if( intensity == 255 ){
+    fadeIn = false;
+    Serial.println("FadeIn");
+  }
+  if( intensity == 0){
+    fadeIn = true;
+    Serial.println("FadeOut");
+    delay(500);
+  }
 }
